075bfba7b63f3cfe314ba3388321f598e98289c7
Vilém Zouhar <vilem.zouhar@gmail.com>
2023-05-15 13:18:44 +0200

    lint

--- src/approve_and_bonus.py
+++ src/approve_and_bonus.py
@@ -8,7 +8,10 @@ import time
 from collections import defaultdict
 import numpy as np
 
-messages = {'exit_screen': "You exited the screen more than three times and this was explicitly forbidden in the instructions.", "too_fast": "You completed the task too quickly indicated you didn't attempt each question seriously.", "no_code": "You didn't complete the experiments"}
+messages = {
+    'exit_screen': "You exited the screen more than three times and this was explicitly forbidden in the instructions.",
+    "too_fast": "You completed the task too quickly indicated you didn't attempt each question seriously.", "no_code": "You didn't complete the experiments"}
+
 
 def get_submissions(study_id):
     r = requests.get(
@@ -48,15 +51,16 @@ def name_check():
 
 def reject_submission(submission_id, participant_id, message, rejection_category):
     if args.dry_run:
-        print(f"Not continuing because of --dry-run but would reject {submission_id} of {participant_id}")
+        print(
+            f"Not continuing because of --dry-run but would reject {submission_id} of {participant_id}")
         return
-    
-    assert rejection_category in ["TOO_QUICKLY", "TOO_SLOWLY" ,"FAILED_INSTRUCTIONS", "INCOMP_LONGITUDINAL", "FAILED_CHECK" ,"LOW_EFFORT",
-                       "MALINGERING" ,"NO_CODE", "BAD_CODE", "NO_DATA", "UNSUPP_DEVICE", "OTHER"] 
+
+    assert rejection_category in ["TOO_QUICKLY", "TOO_SLOWLY", "FAILED_INSTRUCTIONS", "INCOMP_LONGITUDINAL", "FAILED_CHECK", "LOW_EFFORT",
+                                  "MALINGERING", "NO_CODE", "BAD_CODE", "NO_DATA", "UNSUPP_DEVICE", "OTHER"]
     r = requests.post(
         f"https://api.prolific.co/api/v1/submissions/{submission_id}/transition/",
         headers={"Authorization": f"Token {API_TOKEN}"},
-        
+
         json={
             "action": "REJECT",
             "message": message,
@@ -73,7 +77,8 @@ def reject_submission(submission_id, participant_id, message, rejection_category
 
 def approve_submission(submission_id, participant_id):
     if args.dry_run:
-        print(f"Not continuing because of --dry-run but would approve {submission_id} of {participant_id}")
+        print(
+            f"Not continuing because of --dry-run but would approve {submission_id} of {participant_id}")
         return
     r = requests.post(
         f"https://api.prolific.co/api/v1/submissions/{submission_id}/transition/",
@@ -92,8 +97,10 @@ def approve_submission(submission_id, participant_id):
 
 def setup_bonuses(approved_participants, study_id):
     if args.dry_run:
-        print("Not continuing because of --dry-run but would set up the following csv bonuses")
-        print("\\n".join([f"{x['worker_id']},{x['bonus']}" for x in approved_participants]))
+        print(
+            "Not continuing because of --dry-run but would set up the following csv bonuses")
+        print("\\n".join(
+            [f"{x['worker_id']},{x['bonus']}" for x in approved_participants]))
         return
 
     r = requests.post(
@@ -110,9 +117,11 @@ def setup_bonuses(approved_participants, study_id):
     print("Set up bonus payment with the following response", d)
     return d["id"]
 
+
 def pay_bonuses(bonus_id):
     if args.dry_run:
-        print(f"Not continuing because of --dry-run but would pay the id {bonus_id}")
+        print(
+            f"Not continuing because of --dry-run but would pay the id {bonus_id}")
         return
 
     r = requests.post(
@@ -123,6 +132,7 @@ def pay_bonuses(bonus_id):
         exit("Failed to pay bonus payment " + r.content.decode())
     d = r.json()
     print("Paid the bonus with the following response", d)
+
 
 args = argparse.ArgumentParser()
 args.add_argument(
@@ -149,14 +159,15 @@ weird_users = []
 for user, datum in user_data.items():
     # print(len(datum))
     if len(datum) != 30:
-        print(f"The user {user} has {len(datum)} entries. Please manually check this!")
+        print(
+            f"The user {user} has {len(datum)} entries. Please manually check this!")
         if user == "63172692591f6b7c1b94af3a":
             continue
         weird_users.append(user)
     if "qno" in datum[0]:
-        #we sort 
+        # we sort
         assert False
-    
+
 
 for user in weird_users:
     user_data.pop(user)
@@ -184,13 +195,13 @@ print(f"Found the following studies")
 for d_study in d_studies:
     print(d_study["name"], d_study["id"], d_study["internal_name"])
 
-
     d_submissions = get_submissions(d_study['id'])
     d_submissions_awaiting = [
         x for x in d_submissions
         if x["status"] == "AWAITING REVIEW"
     ]
-    print(f"Fetched {len(d_submissions)} submissions out of which {len(d_submissions_awaiting)} are awaiting review")
+    print(
+        f"Fetched {len(d_submissions)} submissions out of which {len(d_submissions_awaiting)} are awaiting review")
 
     pid_to_sid = {x["participant_id"]: x["id"] for x in d_submissions}
     pid_to_sid_awaiting = {
@@ -198,12 +209,8 @@ for d_study in d_studies:
         for x in d_submissions_awaiting
     }
 
-
-
-
     users_waiting_to_be_approved = []
     users_waiting_to_be_rejected = []
-
 
     current_study_data = study_user_data[d_study['id']]
     for user, datum in current_study_data.items():
@@ -216,21 +223,23 @@ for d_study in d_studies:
                 print(
                     f"The prolific participant {user} has already been approved, skipping.\nPlease don't include them in the control file so that nobody gets paid twice.")
         else:
-            ## Rejection criteria exit screen > 3
+            # Rejection criteria exit screen > 3
             exit_screen = sum([int(d['count_exited_page']) for d in datum]) > 3
-            ## Rejection criteria too fast
-            too_fast = np.mean([sum([t for t in d['times'].values()]) for d in datum]) < 3000
+            # Rejection criteria too fast
+            too_fast = np.mean([sum([t for t in d['times'].values()])
+                               for d in datum]) < 3000
             if exit_screen:
-                users_waiting_to_be_rejected.append({'id':user, 'reason':messages['exit_screen'], "rejection_category":"FAILED_INSTRUCTIONS", "session_id":datum[0]['url_data']['session_id']})
+                users_waiting_to_be_rejected.append(
+                    {'id': user, 'reason': messages['exit_screen'], "rejection_category": "FAILED_INSTRUCTIONS", "session_id": datum[0]['url_data']['session_id']})
             elif too_fast:
-                users_waiting_to_be_rejected.append({'id':user, 'reason':messages['too_fast'], "rejection_category":"TOO_QUICKLY",  "session_id":datum[0]['url_data']['session_id']})
+                users_waiting_to_be_rejected.append(
+                    {'id': user, 'reason': messages['too_fast'], "rejection_category": "TOO_QUICKLY", "session_id": datum[0]['url_data']['session_id']})
             else:
-                #Accept
-                users_waiting_to_be_approved.append({"id":user, "bonus":datum[-1]['user_balance'],  "session_id":datum[0]['url_data']['session_id']})
+                # Accept
+                users_waiting_to_be_approved.append(
+                    {"id": user, "bonus": datum[-1]['user_balance'], "session_id": datum[0]['url_data']['session_id']})
 
     print(f"In study {d_study['id']} we have {len(users_waiting_to_be_approved)} participants to approve and {len(users_waiting_to_be_rejected)} to reject")
-
-
 
     print(
         f"Will reject {len(users_waiting_to_be_rejected)} participants"
@@ -248,13 +257,14 @@ for user in users_waiting_to_be_approved:
     approve_submission(user["session_id"], user["id"])
     with open("data/local_ledger_approved.jsonl", "a") as f:
         f.write(json.dumps(user) + "\n")
-    
-    
+
+
 for user in users_waiting_to_be_rejected:
     # throttle because cancelling mid-payout is horrible (I wish there were transactions in Prolific API)
     time.sleep(1)
 
-    reject_submission(user["session_id"], user["id"], user["rejection_category"], user["reason"])
+    reject_submission(user["session_id"], user["id"],
+                      user["rejection_category"], user["reason"])
     with open("data/local_ledger_rejected.jsonl", "a") as f:
         f.write(json.dumps(user) + "\n")
 
