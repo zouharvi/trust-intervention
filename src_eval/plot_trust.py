b1513d174396d6ee5928fbb1c3c03e13bca31335
Vilém Zouhar <vilem.zouhar@gmail.com>
2023-05-16 15:02:28 +0200

    nicer figures

--- src_eval/plot_trust.py
+++ src_eval/plot_trust.py
@@ -51,8 +51,17 @@ QUEUE_PLAN_BARS = {
         5 * ["vague"] +
         []
     ),
+    "intervention_uc_no_vague": (
+        10 * ["calibrated"] +
+        5 * ["unconf. corr."] +
+        15 * ["calibrated"] +
+        []
+    ),
 }
 QUEUE_PLAN_NAMES = collections.defaultdict(str)
+QUEUE_PLAN_NAMES["intervention_uc_no_vague"] = "Unconfidently Correct"
+QUEUE_PLAN_NAMES["intervention_ci_no_vague"] = "Confidently Incorrect"
+QUEUE_PLAN_NAMES["control_no_vague"] = "Control"
 
 args = argparse.ArgumentParser()
 args.add_argument("-q", "--queue", default="control_no_vague")
@@ -62,7 +71,7 @@ args = args.parse_args()
 data_by_user = utils.load_data(args.data, args.queue)
 
 print(
-    f"{len(data_by_user)} users with {np.average([len(x) for x in data_by_user]):.0f} questions"
+    f"{len(data_by_user)} users with {np.average([len(x) for x in data_by_user]):.1f} questions"
 )
 
 bet_vals = [[] for _ in range(30)]
@@ -82,7 +91,20 @@ for data_local in data_by_user:
             data_local[i]["user_decision"] == data_local[i]["question"]["ai_is_correct"]
         )
 
-fig = plt.figure(figsize=(5, 2))
+fig = plt.figure(figsize=(4.5, 2))
+xticks_fine = np.linspace(0, 30, 500)
+
+# plot histogram
+im_correctness = np.array([
+    [np.average(user_correct) for user_correct in user_correct]
+])
+im_correctness = resize_img(im_correctness, (15, 305))
+fig.figimage(
+    X=im_correctness, xo=61, yo=fig.bbox.ymax - 37,
+    cmap="RdYlGn", vmin=0.2, vmax=1
+)
+
+# plot points
 plt.scatter(
     range(30),
     [np.average(bet_val) for bet_val in bet_vals],
@@ -91,9 +113,7 @@ plt.scatter(
     edgecolor="black"
 )
 
-
-xticks_fine = np.linspace(0, 30, 500)
-
+# plot line
 poly_fit = np.poly1d(np.polyfit(
     range(30), [np.average(bet_val) for bet_val in bet_vals], 3
 ))
@@ -101,31 +121,22 @@ plt.plot(
     xticks_fine, poly_fit(xticks_fine), '-', color="black", zorder=-100
 )
 
-plt.ylim(0.06, 0.15)
+plt.ylim(0.05, 0.15)
 plt.clim(0.2, 1)
 plt.colorbar(label="User Decision Correctness")
 plt.xticks(
     range(0, 30, 5),
     [
-        x if x_i % 2 == 0 else "\n" + x
+        "         " + (x if x_i % 2 == 0 else "\n" + x)
         for x_i, x in enumerate(QUEUE_PLAN_BARS[args.queue][0::5])
     ],
     linespacing=0.6
-    # rotation=90
 )
 
-im_correctness = np.array([
-    [np.average(user_correct) for user_correct in user_correct]
-])
-im_correctness = resize_img(im_correctness, (15, 400 - 51))
-fig.figimage(
-    X=im_correctness, xo=61, yo=fig.bbox.ymax - 22,
-    cmap="RdYlGn", vmin=0.2, vmax=1
-)
 
 BET_VALS = [i / 5 * 0.15 for i in range(5 + 1)]
 plt.yticks(BET_VALS[2:], BET_VALS[2:])
-# plt.title(f"Queue: {args.queue}")
+plt.title(QUEUE_PLAN_NAMES[args.queue])
 plt.ylabel("Trust (bet value)")
 # plt.xlabel("Question+Confidence Setup")
 plt.tight_layout(pad=0)
