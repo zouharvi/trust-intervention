1f66fec62d4cb09c58c8f5f095254a5e274c3cc5
Vilém Zouhar <vilem.zouhar@gmail.com>
2023-05-17 15:35:52 +0200

    new loader

--- src/baked_queues/generate_fake.py
+++ src/baked_queues/generate_fake.py
@@ -14,7 +14,7 @@ import random
 import copy
 
 args = ArgumentParser()
-args.add_argument("-f", "--fakefile", default="data/fake_questions.txt")
+args.add_argument("-d", "--data", default="data/trivia_questions.json")
 args.add_argument("-p", "--plan", default="control")
 args.add_argument("-uc", "--uid-count", default=0, type=int)
 args.add_argument("-s", "--seed", default=0, type=int)
@@ -22,14 +22,16 @@ args = args.parse_args()
 
 random.seed(args.seed)
 
-data_raw = list(open(args.fakefile, "r").readlines())
-data = []
-for q_i in range(len(data_raw) // 4 + 1):
-    data.append((
-        data_raw[q_i * 4 + 0].removesuffix("\n"),
-        data_raw[q_i * 4 + 1].removesuffix("\n").replace("A1: ", ""),
-        data_raw[q_i * 4 + 2].removesuffix("\n").replace("A2: ", ""),
-    ))
+# fake data (old) data/fake_questions.txt
+# data_raw = list(open(args.fakefile, "r").readlines())
+# data = []
+# for q_i in range(len(data_raw) // 4 + 1):
+#     data.append((
+#         data_raw[q_i * 4 + 0].removesuffix("\n"),
+#         data_raw[q_i * 4 + 1].removesuffix("\n").replace("A1: ", ""),
+#         data_raw[q_i * 4 + 2].removesuffix("\n").replace("A2: ", ""),
+#     ))
+data = json.load(open(args.data, "r"))["trivia"]
 
 
 def decide_truthfulness_base(question):
@@ -38,8 +40,8 @@ def decide_truthfulness_base(question):
         0.45, 0.8) if ai_is_correct else random.uniform(0.2, 0.55)
 
     return {
-        "question": question[0],
-        "answer": question[1] if ai_is_correct else question[2],
+        "question": question["question"],
+        "answer": question["correct_answer"] if ai_is_correct else question["incorrect_answer"],
         "ai_is_correct": ai_is_correct,
         "ai_confidence": f"{ai_confidence:.0%}",
     }
@@ -51,32 +53,32 @@ def decide_truthfulness_vague(question):
         0.45, 0.55) if ai_is_correct else random.uniform(0.4, 0.5)
 
     return {
-        "question": question[0],
-        "answer": question[1] if ai_is_correct else question[2],
+        "question": question["question"],
+        "answer": question["correct_answer"] if ai_is_correct else question["incorrect_answer"],
         "ai_is_correct": ai_is_correct,
         "ai_confidence": f"{ai_confidence:.0%}",
     }
 
 
 def decide_truthfulness_ci(question):
-    ai_is_correct = random.choices([True, False], weights=[0.1, 0.9], k=1)[0]
+    ai_is_correct = random.choices([True, False], weights=[0.01, 0.99], k=1)[0]
     ai_confidence = random.uniform(0.7, 1.0)
 
     return {
-        "question": question[0],
-        "answer": question[1] if ai_is_correct else question[2],
+        "question": question["question"],
+        "answer": question["correct_answer"] if ai_is_correct else question["incorrect_answer"],
         "ai_is_correct": ai_is_correct,
         "ai_confidence": f"{ai_confidence:.0%}",
     }
 
 
 def decide_truthfulness_uc(question):
-    ai_is_correct = random.choices([True, False], weights=[0.9, 0.1], k=1)[0]
-    ai_confidence = random.uniform(0.1, 0.4)
+    ai_is_correct = random.choices([True, False], weights=[0.99, 0.01], k=1)[0]
+    ai_confidence = random.uniform(0.0, 0.3)
 
     return {
-        "question": question[0],
-        "answer": question[1] if ai_is_correct else question[2],
+        "question": question["question"],
+        "answer": question["correct_answer"] if ai_is_correct else question["incorrect_answer"],
         "ai_is_correct": ai_is_correct,
         "ai_confidence": f"{ai_confidence:.0%}",
     }
@@ -84,77 +86,21 @@ def decide_truthfulness_uc(question):
 
 QUEUE_PLAN = {
     # control
-    "control": (
-        5 * [decide_truthfulness_base] +
-        5 * [decide_truthfulness_vague] +
-        5 * [decide_truthfulness_base] +
-        5 * [decide_truthfulness_vague] +
-        5 * [decide_truthfulness_base] +
-        5 * [decide_truthfulness_vague] +
+    "control_large": (
+        60 * [decide_truthfulness_base] +
         []
     ),
-    "control_no_vague": (
-        30 * [decide_truthfulness_base] +
-        []
-    ),
-
     # confidently incorrect
-    "intervention_ci": (
-        5 * [decide_truthfulness_base] +
-        5 * [decide_truthfulness_vague] +
-        5 * [decide_truthfulness_ci] +
-        5 * [decide_truthfulness_vague] +
-        5 * [decide_truthfulness_base] +
-        5 * [decide_truthfulness_vague] +
-        []
-    ),
-    "intervention_ci_no_vague": (
+    "intervention_ci_large": (
         10 * [decide_truthfulness_base] +
         5 * [decide_truthfulness_ci] +
-        15 * [decide_truthfulness_base] +
+        45 * [decide_truthfulness_base] +
         []
     ),
-
-    # unconfidently correct
-    "intervention_uc": (
-        5 * [decide_truthfulness_base] +
-        5 * [decide_truthfulness_vague] +
-        5 * [decide_truthfulness_uc] +
-        5 * [decide_truthfulness_vague] +
-        5 * [decide_truthfulness_base] +
-        5 * [decide_truthfulness_vague] +
-        []
-    ),
-    "intervention_uc_no_vague": (
-        10 * [decide_truthfulness_base] +
-        5 * [decide_truthfulness_uc] +
-        15 * [decide_truthfulness_base] +
-        []
-    ),
-
-    # discept pilot
-    "discept_pilot_0": (
-        1 * [decide_truthfulness_base] +
-        1 * [decide_truthfulness_vague] +
-        1 * [decide_truthfulness_ci] +
-        1 * [decide_truthfulness_vague] +
-        1 * [decide_truthfulness_base] +
-        1 * [decide_truthfulness_vague] +
-        []
-    )
 }
 
 UIDs = [
     "demo",
-    # "harare", "lusaka", "sahara", "cardiff", "hanoi",
-    # "caracas", "montevideo", "washington", "kampala", "funafuti",
-    # "ashgabat", "ankara", "tiraspol", "lome", "bangkok",
-    # "dodoma", "dushanbe", "damascus", "bern", "stockholm",
-    # "paramaribo", "khartoum", "madrid", "juba", "seoul",
-    # "pretoria", "hargeisa", "mogadishu", "honiara", "ljubljana",
-    # "bratislava", "philipsburg", "singapore", "freetown", "belgrade",
-    # "zimbabwe", "zambia", "yemen", "wales", "venezuela",
-    # "vietnam", "vanuatu", "uzbekistan", "uruguay", "uganda", "tuvalu"
 ]
 
 for uid in list(range(args.uid_count)) + UIDs:
@@ -169,3 +115,66 @@ for uid in list(range(args.uid_count)) + UIDs:
         uid = f"{uid:0>3}"
     with open(f"src_ui/web/baked_queues/{args.plan}_{uid}.json", "w") as f:
         json.dump(queue, f, indent=4, ensure_ascii=False)
+
+
+# QUEUE_PLAN = {
+#     # control
+#     "control": (
+#         5 * [decide_truthfulness_base] +
+#         5 * [decide_truthfulness_vague] +
+#         5 * [decide_truthfulness_base] +
+#         5 * [decide_truthfulness_vague] +
+#         5 * [decide_truthfulness_base] +
+#         5 * [decide_truthfulness_vague] +
+#         []
+#     ),
+#     "control_no_vague": (
+#         30 * [decide_truthfulness_base] +
+#         []
+#     ),
+
+#     # confidently incorrect
+#     "intervention_ci": (
+#         5 * [decide_truthfulness_base] +
+#         5 * [decide_truthfulness_vague] +
+#         5 * [decide_truthfulness_ci] +
+#         5 * [decide_truthfulness_vague] +
+#         5 * [decide_truthfulness_base] +
+#         5 * [decide_truthfulness_vague] +
+#         []
+#     ),
+#     "intervention_ci_no_vague": (
+#         10 * [decide_truthfulness_base] +
+#         5 * [decide_truthfulness_ci] +
+#         15 * [decide_truthfulness_base] +
+#         []
+#     ),
+
+#     # unconfidently correct
+#     "intervention_uc": (
+#         5 * [decide_truthfulness_base] +
+#         5 * [decide_truthfulness_vague] +
+#         5 * [decide_truthfulness_uc] +
+#         5 * [decide_truthfulness_vague] +
+#         5 * [decide_truthfulness_base] +
+#         5 * [decide_truthfulness_vague] +
+#         []
+#     ),
+#     "intervention_uc_no_vague": (
+#         10 * [decide_truthfulness_base] +
+#         5 * [decide_truthfulness_uc] +
+#         15 * [decide_truthfulness_base] +
+#         []
+#     ),
+
+#     # discept pilot
+#     "discept_pilot_0": (
+#         1 * [decide_truthfulness_base] +
+#         1 * [decide_truthfulness_vague] +
+#         1 * [decide_truthfulness_ci] +
+#         1 * [decide_truthfulness_vague] +
+#         1 * [decide_truthfulness_base] +
+#         1 * [decide_truthfulness_vague] +
+#         []
+#     )
+# }
